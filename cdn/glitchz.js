fc00e97eb94fa8115b42ecbcee124b9aa8f4e96d
Aida☄☿ <37634577+4ida@users.noreply.github.com>
2021-08-19 16:05:11 +0300

    commission by @AnorakTrendtru (alt ver)

--- cdn/glitchz.js
+++ cdn/glitchz.js
@@ -2,7 +2,7 @@
   let $context = $canvas.getContext('2d');
   let img = new Image();
   //img.src = '/cdn/img/lain.jpg';
-  img.src = '/cdn/img/portrait/4468a2e0baac76335deb62f6bfbe25e7.png';
+  img.src = '/cdn/img/portrait/sketch-1629376216474.png';
 
   let w, h, offset, glitchInterval;
 
